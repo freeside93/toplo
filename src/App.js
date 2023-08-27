b592866b78039cc0adb8d8db9c853737d6339f79
freudside <89998153+freudside@users.noreply.github.com>
2023-08-27 13:25:47 +0300

    small changes on footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Projects from './components/Projects';
 import Contacts from './components/Contacts'
 import { useState } from 'react';
 import toploLogo5 from '../src/images/toplo_logo_5.png'
+import reactivLogo from '../src/images/reactivLogo.png'
+import {FiPhoneCall} from 'react-icons/fi'
 
 function App() {
   const [currentPage, setCurrentPage] = useState('projects')
@@ -47,14 +49,17 @@ function App() {
         {currentPage === 'about us' && <AboutUs />}
         {currentPage === 'contacts' && <Contacts />}
       </main>
-      <footer className='flex flex-row justify-center border-orange-400 border-b-2  mt-2'>
-        <div>
-          
+      <footer className='flex flex-row justify-items-center align-middle border-orange-400 border-b-2 text-sm  mt-4'>
+        <div className='w-2/12 m-auto max-w-[100px] '>
+              <img src={reactivLogo}  className='cursor-pointer'/>
         </div>
-        <div>
-          {currentPage === 'projects' && <h1 className='text-lg font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е УЮТ!</h1>}
-          {currentPage === 'about us' && <h1 className='text-lg font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е СПОКОЙСТВИЕ!</h1>}
-          {currentPage === 'about us' && <h1 className='text-lg font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е КОМФОРТ!</h1>}
+        <div className='m-auto w-8/12 flex justify-center'> 
+          {currentPage === 'projects' && <h1 className=' font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е УЮТ!</h1>}
+          {currentPage === 'about us' && <h1 className=' font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е СПОКОЙСТВИЕ!</h1>}
+          {currentPage === 'contacts' && <h1 className=' font-bold italic text-orange-400 mt-2 drop-shadow-md shadow-orange-400'>ЗAЩОТО ТОПЛИНАТА Е КОМФОРТ!</h1>}
+        </div>
+        <div className='flex flex-row w-2/12 m-auto '>
+           <a href="tel:+359897462492" className='m-auto w-full'><FiPhoneCall  className=' text-green-400 border-2 border-green-800 rounded-full p-1 m-auto' size ={30} /></a>
         </div>
       </footer>
     </div>
